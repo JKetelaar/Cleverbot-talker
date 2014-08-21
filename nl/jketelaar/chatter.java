3e5349fb08088c2b166aee790e13ac315b9668f3
Jeroen Ketelaar <JKetelaar@users.noreply.github.com>
2014-08-21 23:50:22 +0200

    Create chatter.java

---
+++ nl/jketelaar/chatter.java
@@ -0,0 +1,13 @@
+package nl.jketelaar.chatter;
+
+public class ChatterBotThought {
+    private String text;
+
+    public String getText() {
+        return this.text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
