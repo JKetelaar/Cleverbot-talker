d925b8762e8759e6b0ff74c27529df7d76891740
Jeroen Ketelaar <JKetelaar@users.noreply.github.com>
2014-08-21 23:51:55 +0200

    Create ChatExample.java

---
+++ ChatExample.java
@@ -0,0 +1,21 @@
+package nl.jketelaar.chatter.main;
+
+import nl.jketelaar.chatter.CleverbotSession;
+
+/**
+ * @author JKetelaar
+ */
+public class ChatExample {
+    public static void main(String[] args) throws Exception {
+
+        CleverbotSession cleverbot = new CleverbotSession("http://www.cleverbot.com/webservicemin", 35);
+
+        String question = "How are you?";
+        String answer = cleverbot.think(question);
+        System.out.println("Bot replies with: " + answer);
+
+        String question2 = "What are you doing?";
+        String answer2 = cleverbot.think(question2);
+        System.out.println("Bot replies with: " + answer2);
+    }
+}
